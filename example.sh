7e6d51ccb2c1b8c6bc084da5c9f40fc5d6a03922
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-11-11 14:53:31 +0100

    Add some examples

---
+++ example.sh
@@ -0,0 +1,6 @@
+S1="s[nil.1]"
+echo "$S1 = `echo $S1 | ./_build/default/runprf.exe`"
+S1="s[nil.10]"
+echo "$S1 = `echo $S1 | ./_build/default/runprf.exe`"
+S1="(rec z (proj 1)) [nil.8]"
+echo "$S1 = `echo $S1 | ./_build/default/runprf.exe`"
