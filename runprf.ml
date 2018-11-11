9bb2721f99a7daea060fbc9620326e72709eb4ca
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-11-11 14:45:14 +0100

    Parse and execute at the top-level

--- runprf.ml
+++ runprf.ml
@@ -1,7 +1,9 @@
-let pred_correct =
-  QCheck.Test.make ~count:1000 ~name:"pred is correct"
-    QCheck.small_int
-    (fun x -> (not (x > 0)) || (PRF.eval PRF.pred) [x] = x - 1);;
+(* let pred_correct = *)
+  (* QCheck.Test.make ~count:1000 ~name:"pred is correct" *)
+    (* QCheck.small_int *)
+    (* (fun x -> (not (x > 0)) || (PRF.eval PRF.pred) [x] = x - 1);; *)
 
 (* we can check right now the property... *)
-QCheck_runner.run_tests ~verbose:true [pred_correct];;
+(* QCheck_runner.run_tests ~verbose:true [pred_correct];; *)
+
+PRF.main ();;
