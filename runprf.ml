8be81efe00e287732d6e7ba060e430789054b498
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-11-11 11:27:54 +0100

    Start adding some quickcheck

--- runprf.ml
+++ runprf.ml
@@ -1,3 +1,7 @@
-open Core.Printf;;
+let pred_correct =
+  QCheck.Test.make ~count:1000 ~name:"pred is correct"
+    QCheck.small_int
+    (fun x -> (not (x > 0)) || (PRF.eval PRF.pred) [x] = x - 1);;
 
-printf "%s\n" "Hi!"
+(* we can check right now the property... *)
+QCheck_runner.run_tests ~verbose:true [pred_correct];;
